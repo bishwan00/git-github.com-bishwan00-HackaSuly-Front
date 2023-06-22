0a8e763f640410e90946797643a4b39cef74b93b
bishwan00 <bishwan.00606094@gmail.com>
2023-06-23 00:10:45 +0300

    adding navbar

---
+++ src/componets/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import PersonIcon from "@mui/icons-material/Person";
+import Logo from "../../../public/assets/Planet_earth.svg";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center gap-10">
+      <div className="mt-[69px] ml-6 w-auto">
+        <PersonIcon style={{ fontSize: 45 }} className="text-gray-700" />
+      </div>
+      <div className="mt-14 ml-10">
+        <img src={Logo} className="w-[71px] h-[71px]" />
+      </div>
+      <div className="mt-[69px] bg-gray-500 p-2 pl-4 rounded-l-3xl">
+        <span className="text-md text-white  ">Donate</span>
+        <FavoriteIcon
+          style={{ fontSize: 25 }}
+          className="text-green-500 ml-2"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

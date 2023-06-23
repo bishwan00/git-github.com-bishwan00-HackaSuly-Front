feeac235a7a8d5c39327fa5f8f4790b968f14a0c
moein9 <moeen.abas1@gmail.com>
2023-06-23 22:49:10 +0300

    payment has been added

--- src/views/Payment.jsx
+++ src/views/Payment.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Tree from "../../public/assets/payment-tree.png";
 import { useState } from "react";
 
+import FibQR from "../../public/assets/FIB.jpeg";
+
 const Payment = () => {
   const [qunatitiy, setQuantity] = useState(1);
   const [dollar, setDolar] = useState(5);
@@ -40,14 +42,38 @@ const Payment = () => {
         </button>
       </div>
 
-      <div className="bg-white rounded-lg mt-10 p-2 mx-4">
-        <div className="flex flex-col justify-center  items-center p-2 m-2">
-          <h1 className="text-2xl ">Payment</h1>
+      <div className="flex  flex-col bg-white rounded-lg mt-10 p-2 mx-4">
+        <div className=" ">
+          <h5 className="flex justify-center items-center align-middle p-2 ">
+            Manual payment
+          </h5>
+        </div>
+
+        <div>
+          <img src={FibQR} alt="" />
+        </div>
+
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="  items-center font-bold ">please send</h1>
+
+          <h1>fullname , email , phone number throgh FIB</h1>
+        </div>
+      </div>
+
+      <div className="bg-white rounded-lg mt-10 mb-20 p-2 mx-4">
+        <div className="flex flex-col justify-center  items-center p-2 m-2 disabled">
+          <h1 className="text-xl ">
+            FIB Payment integration <br />
+            <b className="text-green-400 rounded-lg justify-center items-center mx-12">
+              coming soon
+            </b>{" "}
+          </h1>
           <div className=" py-4 w-full">
             <label className="block text-sm mb-2" htmlFor="">
               card number
             </label>
             <input
+              disabled
               className="appearance-none rounded-md   w-full px-3 py-2 border border-gray-300 placeholder-gray-500   focus:outline-none focus:ring-agreen-500 focus:border-agreen-500 sm:text-sm"
               id="name"
               name="name"
@@ -61,6 +87,7 @@ const Payment = () => {
                 Expires
               </label>
               <input
+                disabled
                 className="appearance-none rounded-md   w-auto px-3 py-2 border border-gray-300 placeholder-gray-500   focus:outline-none focus:ring-agreen-500 focus:border-agreen-500 sm:text-sm"
                 id="name"
                 name="name"
@@ -73,6 +100,7 @@ const Payment = () => {
                 CVV
               </label>
               <input
+                disabled
                 className="appearance-none rounded-md   w-full px-3 py-2 border border-gray-300 placeholder-gray-500   focus:outline-none focus:ring-agreen-500 focus:border-agreen-500 sm:text-sm"
                 id="name"
                 name="name"
@@ -81,7 +109,7 @@ const Payment = () => {
               />
             </div>
           </div>
-          <div className=" flex flex-col items-center w-full mt-3">
+          <div disabled className=" flex flex-col items-center w-full mt-3">
             <button
               type="submit"
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent  rounded-md text-white bg-agreen-600 hover:bg-agreen-500 focus:outline-none focus:ringfocus:ring-agreen-500 focus:ring-offset-2 focus:ring-2"

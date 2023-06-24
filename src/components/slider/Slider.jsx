0f385b1f62362aa6870201850647b4782eb0fdd0
alaaaand <alandmustafa4@gmail.com>
2023-06-24 12:06:19 +0300

    fixing sizing of slides components images

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -5,33 +5,35 @@ const contentStyle = {
   lineHeight: "160px",
   textAlign: "center",
   background: "#364d79",
+  
 };
 const Slider = () => (
   <Carousel autoplay dots={false} autoplaySpeed={5000} effect="scrollx">
     <div>
       <img
-        className={`${contentStyle} rounded-xl`}
-        src="../../../public/assets/test.png"
+        className={`${contentStyle} rounded-xl h-72 w-full object-cover`}
+        src="../../../public/assets/nawroz.jpg"
       />
     </div>
     <div>
       <img
-        className={`${contentStyle} rounded-xl`}
-        src="../../../public/assets/test.png"
-      />{" "}
+        className={`${contentStyle} rounded-xl h-72 w-full object-cover`}
+        src="../../../public/assets/greenn.jpg"
+      />
     </div>
     <div>
       <img
-        className={`${contentStyle} rounded-xl`}
-        src="../../../public/assets/test.png"
-      />{" "}
+        className={`${contentStyle} rounded-xl h-72 w-full object-cover`}
+        src="../../../public/assets/green.jpg"
+      />
     </div>
     <div>
       <img
-        className={`${contentStyle} rounded-xl`}
-        src="../../../public/assets/test.png"
-      />{" "}
+        className={`${contentStyle} rounded-xl h-72 w-full object-cover`}
+        src="../../../public/assets/nawroz.jpg"
+      />
     </div>
   </Carousel>
 );
+
 export default Slider;

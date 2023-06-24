7a8bc06ce578e54c754b3f3595c0bdbe9bdaa8ab
alaaaand <alandmustafa4@gmail.com>
2023-06-24 13:35:00 +0300

    arranging some positions

--- src/components/statistic/Statistic.jsx
+++ src/components/statistic/Statistic.jsx
@@ -2,7 +2,7 @@ const Statistic = () => {
   return (
     <div className="flex flex-col rounded-xl bg-white  my-10">
       <div className="flex gap-x-2 px-3 py-6 border-b justify-center items-center">
-        <div className="text-2xl font-bold text-center  w-20">$1200</div>
+        <div className="text-2xl font-bold text-center  w-24">$1200</div>
         <div className="text-md ">Donated This Month</div>
       </div>
       <div className="flex gap-x-2 px-3 py-6 border-b justify-center items-center">
@@ -11,10 +11,10 @@ const Statistic = () => {
       </div>{" "}
       <div className="flex gap-x-6 px-3 py-6 border-b justify-center items-center">
         <div className="text-2xl font-bold text-orange-400 w-20  ">2369</div>
-        <div className="text-md ">Green Lovers</div>
+        <div className="text-md mr-2">Green Lovers</div>
       </div>{" "}
       <div className="flex gap-x-2 px-3 py-6  border-b justify-center items-center">
-        <div className="text-2xl font-bold  w-20 text-agreen-500 ">3</div>
+        <div className="text-2xl font-bold  w-16 text-agreen-500 ml-12 ">3</div>
         <div className="text-md   ">Live Challenges </div>{" "}
         <span className="w-2 h-2 rounded-full bg-green-500 mr-2"></span>
       </div>

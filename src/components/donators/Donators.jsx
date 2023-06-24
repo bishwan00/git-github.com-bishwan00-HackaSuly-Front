bb9ec8e4aef26c415f5ea4fe02f61a63e504f566
Bishwan Sheko <85481624+bishwan00@users.noreply.github.com>
2023-06-24 11:07:24 +0300

    finall

--- src/components/donators/Donators.jsx
+++ src/components/donators/Donators.jsx
@@ -2,84 +2,47 @@ import React from "react";
 import image from "../../../public/assets/donators.png";
 import { useState } from "react";
 import { Carousel } from "antd";
+import { yellow } from "color-name";
 
-const Donators = () => {
+const Donators = ({ users }) => {
   const [price, setPrice] = useState(750);
   const [companyName, setCompanyName] = useState("Suly company");
 
-  const data = [
-    {
-      image: image,
-      price: 400,
-      companyName: "MAHMUD RICE",
-    },
-    {
-      image: image,
-      price: 200,
-      companyName: "POTAN",
-    },
-    {
-      image: image,
-      price: 100,
-      companyName: "ALAND company",
-    },
-  ];
-
   return (
     <Carousel autoplay dots={false} autoplaySpeed={3000} effect="scrollx">
-      <div className="flex gap-4 items-center my-4 rounded-xl  bg-yellow-500">
-        <div className="w-80">
-          <div className="flex gap-4 p-4 items-center my-4 rounded-xl ">
-            <div className="w-20 ml-2">
-              <img src={data[0].image} className="rounded-2xl" alt="img" />
-            </div>
-            <div className="flex flex-col gap-2">
-              <div className="text-white text-md font-bold ml-2 text-2xl">
-                ${data[0].price}
-              </div>
-              <div className="text-white text-md font-bold justify-center items-center ml-2">
-                {data[0].companyName}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="flex gap-4 items-center my-4 rounded-xl  bg-gray-400">
-        <div className="w-80">
-          <div className="flex gap-4 p-4 items-center my-4 rounded-xl ">
-            <div className="w-20 ml-2">
-              <img src={data[1].image} className="rounded-2xl" alt="img" />
-            </div>
-            <div className="flex flex-col gap-2">
-              <div className="text-white text-md font-bold ml-2 text-2xl">
-                ${data[1].price}
-              </div>
-              <div className="text-white text-md font-bold ml-2">
-                {data[1].companyName}
+      {users?.data?.map((user, index) => {
+        return (
+          <div
+            className={`flex gap-4 items-center my-4 rounded-xl ${
+              index === 0
+                ? "bg-yellow-500"
+                : index === 1
+                ? "bg-gray-400"
+                : "bg-orange-500"
+            } `}
+          >
+            <div className="w-80">
+              <div className="flex gap-4 p-4 items-center my-4 rounded-xl ">
+                <div className="w-20 ml-2">
+                  <img
+                    src={`http://localhost:4000/uploads/${user?.image}`}
+                    className="rounded-2xl"
+                    alt="img"
+                  />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <div className="text-white text-md font-bold ml-2 text-2xl">
+                    ${user?.donate}
+                  </div>
+                  <div className="text-white text-md font-bold ml-2">
+                    {user?.firstName}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-
-      <div className="flex gap-4 items-center my-4 rounded-xl  bg-orange-500">
-        <div className="w-80">
-          <div className="flex gap-4 p-4 items-center my-4 rounded-xl ">
-            <div className="w-20 ml-2">
-              <img src={data[2].image} className="rounded-2xl" alt="img" />
-            </div>
-            <div className="flex flex-col gap-2">
-              <div className="text-white text-md font-bold ml-2 text-2xl">
-                ${data[2].price}
-              </div>
-              <div className="text-white text-md font-bold ml-2">
-                {data[2].companyName}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+        );
+      })}
     </Carousel>
   );
 };
--- src/views/Stats.jsx
+++ src/views/Stats.jsx
@@ -4,7 +4,11 @@ import Slider from "../components/slider/Slider";
 import SuperHero from "../components/superHeros/SuperHero";
 import proImg from "../../public/assets/propic.jpeg";
 import Donators from "../components/donators/Donators";
+import { useGetAllUsersQuery, useGetAllUsersWinnerQuery } from "../api/user";
 const Stats = () => {
+  const { data: users } = useGetAllUsersQuery({ limit: 3 });
+  const { data: usersWiner } = useGetAllUsersWinnerQuery({ limit: 3 });
+
   return (
     <Container>
       <Slider />
@@ -12,11 +16,18 @@ const Stats = () => {
         Last Month Top Donators
       </h1>
 
-      <Donators />
+      <Donators users={users} />
       <h1 className="text-center  text-gray-500 ">Last Month Superheros</h1>
-      <SuperHero name="Bishwan Sherko" image={proImg} price={100} />
-      <SuperHero name="Bishwan Sherko" image={proImg} price={100} />
-      <SuperHero name="Bishwan Sherko" image={proImg} price={100} />
+      {usersWiner?.data?.map((e) => {
+        return (
+          <SuperHero
+            name={`${e?.firstName} ${e?.lastName}`}
+            image={`http://localhost:4000/uploads/${e?.image}`}
+            price={e?.point}
+          />
+        );
+      })}
+
       <Statistic />
     </Container>
   );
